0f81ee89ae6bb5da97f1c5f8015d77797f0ad4f1
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-09-22 22:23:45 -0300

    Realizando ajustes no grid  de resultados para tela de cadastro de eventos.

--- frontend/gestao-eventos-esportivos/src/Components/GridResultado.jsx
+++ frontend/gestao-eventos-esportivos/src/Components/GridResultado.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import formatDate from "../hooks/formatDate";
+import getDataAtual from "../hooks/getDataAtual";
+import {
+  EyeIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/solid";
 
 export default () => {
   const [eventos, setEventos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const dataAtual = getDataAtual();
 
+  //CHAMANDO API PARA PEGAR OS EVENTOS CADASTRADOS
   useEffect(() => {
     const token = window.localStorage.getItem("token");
     const eventos = async () => {
@@ -23,7 +31,6 @@ export default () => {
 
     eventos();
   }, []);
-  //CHAMANDO API PARA PEGAR OS EVENTOS CADASTRADOS
 
   return (
     <div className="px-48 pt-24">
@@ -33,8 +40,7 @@ export default () => {
             Eventos
           </h1>
           <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
+            Segue abaixo a lista de todos os eventos cadastrados.
           </p>
         </div>
         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
@@ -54,7 +60,7 @@ export default () => {
                 <tr>
                   <th
                     scope="col"
-                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-4"
                   >
                     Nome Evento
                   </th>
@@ -74,7 +80,13 @@ export default () => {
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
-                    Data do Evento
+                    Data Inicio
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Data Encerramento
                   </th>
                   <th
                     scope="col"
@@ -82,47 +94,95 @@ export default () => {
                   >
                     Status
                   </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Ações
+                  </th>
                   <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                     <span className="sr-only">Edit</span>
                   </th>
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
-                {eventos.map((evento) => (
-                  <tr key={evento.id}>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="font-medium text-gray-900">
-                            {evento.nomeEvento}
+                {eventos.map((evento) => {
+                  //VARIÁVEIS PARA VALIDAÇÃO
+                  const dataInicialEventoFormatada = formatDate(
+                    evento.dataInicial
+                  );
+                  const dataFinalEventoFormatada = formatDate(evento.dataFinal);
+                  //COMPARAÇÃO DAS DATAS
+                  const isNaoIniciado = dataAtual < dataInicialEventoFormatada;
+                  const isEmAndamento =
+                    dataAtual >= dataInicialEventoFormatada &&
+                    dataAtual <= dataFinalEventoFormatada;
+                  const isEncerrado = dataAtual > dataFinalEventoFormatada;
+                  //DETERMINANDO TEXTO E A CLASSE A SER EXIBIDA
+                  let statusTexto, statusClasse;
+                  if (isNaoIniciado) {
+                    statusTexto = "Não Iniciado";
+                    statusClasse =
+                      "inline-flex items-center rounded-md bg-yellow-200 px-2 py-1 text-xs font-medium text-yellow-700 ring-1 ring-inset ring-yellow-600/20";
+                  } else if (isEmAndamento) {
+                    statusTexto = "Em Andamento";
+                    statusClasse =
+                      "inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20";
+                  } else if (isEncerrado) {
+                    statusTexto = "Encerrado";
+                    statusClasse =
+                      "inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20";
+                  }
+                  //--//
+                  return (
+                    <tr key={evento.id}>
+                      <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                        <div className="flex items-center">
+                          <div className="ml-4">
+                            <div className="font-medium text-gray-900">
+                              {evento.nomeEvento}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">{evento.local}</div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      {`${evento.quantidadeEquipes} - jogadores`}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      {formatDate(evento.dataInicial)}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                        Active
-                      </span>
-                    </td>
-                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit<span className="sr-only">, {evento.name}</span>
-                      </a>
-                    </td>
-                  </tr>
-                ))}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                        <div className="text-gray-900">{evento.local}</div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-900">
+                        {`${evento.quantidadeEquipes} - jogadores`}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-900">
+                        {formatDate(evento.dataInicial)}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-900">
+                        {formatDate(evento.dataFinal)}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                        <span className={statusClasse}>{statusTexto}</span>
+                      </td>
+                      <td className="flex gap-3 relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                        <a
+                          href="#"
+                          className="text-[#26AB3B] hover:text-green-700"
+                        >
+                          <EyeIcon className="h-5 w-5" />
+                        </a>
+                        <a
+                          href="#"
+                          className="text-[#2193F3] hover:text-indigo-900"
+                        >
+                          <PencilSquareIcon className="h-5 w-5" />
+                        </a>
+                        <a
+                          href="#"
+                          className="text-[#D40606] hover:text-red-900"
+                        >
+                          <TrashIcon className="h-5 w-5" />
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
--- frontend/gestao-eventos-esportivos/src/hooks/formatDate.js
+++ frontend/gestao-eventos-esportivos/src/hooks/formatDate.js
@@ -1,7 +1,7 @@
 export default (dateString) => {
   const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Janeiro é 0
-  const year = date.getFullYear();
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // getUTCMonth() retorna de 0 a 11
+  const year = date.getUTCFullYear();
   return `${day}-${month}-${year}`;
 };
