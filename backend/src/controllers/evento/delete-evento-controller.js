304b997e1214af6f4e844ab3e7735110e5f7537b
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-11-03 12:16:04 -0300

    Realizando tratamento de chave estrangeira referente a evento.

--- backend/src/controllers/evento/delete-evento-controller.js
+++ backend/src/controllers/evento/delete-evento-controller.js
@@ -2,13 +2,19 @@ import { z } from "zod";
 import DeleteEventoService from "../../services/evento/delete-evento-service.js";
 
 export default class DeleteEventoController {
-  async handle(req, reply) {
-    const paramsSchema = z.object({
-      id: z.coerce.number(),
-    });
-    const { id } = paramsSchema.parse(req.params);
-    const service = new DeleteEventoService();
-    await service.execute({ id });
-    return reply.status(200).send();
-  }
+    async handle(req, reply) {
+        const paramsSchema = z.object({
+            id: z.coerce.number(),
+        });
+        const { id } = paramsSchema.parse(req.params);
+        const service = new DeleteEventoService();
+        try {
+            await service.execute({ id });
+            return reply.status(200).send();
+        } catch (error) {
+            if (error.message.includes("Existe uma equipe vinculada a este evento.")) {
+                return reply.status(404).send({ error: error.message });
+            }
+        }
+    }
 }
--- backend/src/services/evento/delete-evento-service.js
+++ backend/src/services/evento/delete-evento-service.js
@@ -1,11 +1,22 @@
+import GenericError from "../../errors/generic-error.js";
 import db from "../../lib/db.js";
 
 export default class DeleteEventoService {
-  async execute({ id }) {
-    await db.evento.delete({
-      where: {
-        id,
-      },
-    });
-  }
+    async execute({ id }) {
+        const dependenteEquipe = await db.equipe.findMany({
+            where: {
+                idEvento: id,
+            },
+        });
+
+        if (dependenteEquipe.length > 0) {
+            throw new GenericError(404, "Existe uma equipe vinculada a este evento.");
+        }
+
+        await db.evento.delete({
+            where: {
+                id,
+            },
+        });
+    }
 }
